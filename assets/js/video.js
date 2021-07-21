d4f13b1a685f79c48cb91ded791c14244400387f
Michael Stewart <michael.stewart@fond.co>
2021-07-21 11:53:22 -0700

    Add ping test for video channel.

--- assets/js/video.js
+++ assets/js/video.js
@@ -18,6 +18,12 @@ let Video = {
     let msgInput = document.getElementById("msg-input")
     let postButton = document.getElementById("msg-submit")
     let vidChannel = socket.channel("videos:" + videoId)
+
+    vidChannel.join()
+      .receive("ok", resp => console.log("joined the video channel", resp))
+      .receive("error", reason => console.log("joined failed", reason))
+
+    vidChannel.on("ping", ({count}) => console.log("PING", count))
   }
 }
 
