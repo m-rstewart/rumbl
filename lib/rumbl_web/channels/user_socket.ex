5ebc6cfb9c5bc9c249931cc8f6f950dd3e243b4c
Michael Stewart <michael.stewart@fond.co>
2021-07-21 22:21:17 -0700

    Verify token in user socket.

--- lib/rumbl_web/channels/user_socket.ex
+++ lib/rumbl_web/channels/user_socket.ex
@@ -1,12 +1,27 @@
 defmodule RumblWeb.UserSocket do
   use Phoenix.Socket
 
+  @max_age 2 * 7 * 24 * 60 * 60
+
   channel "videos:*", RumblWeb.VideoChannel
 
   @impl true
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  def connect(%{"token" => token}, socket, _connect_info) do
+    case Phoenix.Token.verify(
+      socket,
+      "user socket",
+      token,
+      max_age: @max_age
+    ) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+
+      {:error, _reason} ->
+        :error
+    end
   end
+
+  def connect(_params, _socket, _connect_info), do: :error
 
   @impl true
   def id(_socket), do: nil
