37fa424d52289e11e896759e9500585e9b98d238
Michael Stewart <michael.stewart@fond.co>
2021-07-23 11:22:04 -0700

    Ensure data sync between client disconnects.

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -134,12 +134,13 @@ defmodule Rumbl.Multimedia do
     |> Repo.insert()
   end
 
-  def list_annotations(%Video{} = video) do
+  def list_annotations(%Video{} = video, since_id \\ 0) do
     Repo.all(
       from a in Ecto.assoc(video, :annotations),
-      order_by: [asc: a.at, asc: a.id],
-      limit: 500,
-      preload: [:user]
+        where: a.id > ^since_id,
+        order_by: [asc: a.at, asc: a.id],
+        limit: 500,
+        preload: [:user]
     )
   end
 
